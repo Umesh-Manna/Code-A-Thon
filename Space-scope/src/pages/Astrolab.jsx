14fb93fc8eb4daa2a6a9daf4bcf5b2e9ba510afd
Umesh-Manna <ume.man.rt24@dypatil.edu>
2025-12-26 18:57:59 +0530

    added Astrolab page and connected with App.jsx

---
+++ Space-scope/src/pages/Astrolab.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Astrolab = () => {
+  return (
+    <div className="min-h-screen bg-[linear-gradient(90deg,#5B7CFA_5%,          #9B5DE5_59%)]">
+      <div className="p-8 text-white">
+        Astrolab
+      </div>
+    </div>
+  )
+}
+
+export default Astrolab
