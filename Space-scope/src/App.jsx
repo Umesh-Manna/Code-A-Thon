01570cbebc91a4678ad1e8d4ae3c989c20b5172f
Umesh-Manna <ume.man.rt24@dypatil.edu>
2026-02-02 19:54:22 +0530

    1. modified and fixed app.jsx, Umesh's website working now. 2. Going to work with the sidebar component

--- Space-scope/src/App.jsx
+++ Space-scope/src/App.jsx
@@ -24,6 +24,23 @@ import Events from './screens/Events.jsx';
 import Dashboard from './screens/Dashboard.jsx';
 import Milestones from './screens/Milestones.jsx';
 
+/* Umesh's imports */
+
+// import './App.css'
+import Astrolab1 from '../Space-scope/src/pages/Astrolab/Astrolab1.jsx' 
+
+import Hurricanes from '../Space-scope/src/pages/Skywatch/Hurricanes.jsx' 
+import { BrowserRouter} from 'react-router-dom'
+import Interactive_maps from '../Space-scope/src/pages/Skywatch/Interactive_maps.jsx'
+import Our_environment from '../Space-scope/src/pages/Skywatch/Our_environment.jsx'
+
+import Satellites from '../Space-scope/src/pages/Skyintel/Satellites/Satellites'
+import Live_sky from '../Space-scope/src/pages/Skyintel/live_sky/Live_sky.jsx'
+import ObjectView from '../Space-scope/src/pages/Skyintel/live_sky/ObjectView.jsx'
+import MoonDashboard from '../Space-scope/src/Lunar_moon/MoonDashboard.jsx'
+
+import SolarDataCardsPage from "../Space-scope/src/Solar_data/pages/SolarDataCardsPage.jsx";
+
 const App = () => {
   return (
     <Routes>
@@ -34,7 +51,42 @@ const App = () => {
       {/* Fallback route */}
       <Route path="/milestones" element={<Milestones />} />
       <Route path="*" element={<Navigate to="/" />} />
+
+      {/* Umesh's imports */}
+
+      {/* astrolab */}
+        <Route path="/astrolab" element={<Astrolab1 />} />
+
+        {/* Sky-watch */}
+        <Route path='/skywatch/hurricanes' element={<Hurricanes />}/>
+        <Route path='/skywatch/interactive_maps' element={<Interactive_maps />}/>
+        <Route path='/skywatch/our_environment' element={<Our_environment />}/>
+
+        {/* skyintel */}
+        <Route path='/skyintel/satellite' element={<Satellites/>}></Route>
+        <Route
+          path="/skyintel/live_sky"
+          element={<Live_sky/>}
+        />
+        <Route
+          path="/skyintel/live_sky/object/:objectId"
+          element={<ObjectView/>}
+        />
+
+        <Route
+          path="/skyintel/lunar_view"
+          element={<MoonDashboard/>}
+        />
+
+        <Route
+          path="/skyintel/solar_data"
+          element={<SolarDataCardsPage />}
+        />
     </Routes>
+
+
+
+    
   );
 };
 
